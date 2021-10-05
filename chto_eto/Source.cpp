d3d129f7e3631532dd961ba3a10002ef84b39fca
ebobola-web <romawot94@gmail.com>
2021-10-05 13:15:12 +0300

    added float

--- chto_eto/Source.cpp
+++ chto_eto/Source.cpp
@@ -1,67 +1,64 @@
 ﻿#include <stdio.h>
-#include <locale.h>
+//#include <locale.h>
+
+#define ROUND_COUNT 2
+#define HOR_SYMBOL '─'
+#define VER_SYMBOL '│'
 
 int number_len(int number);
 void print_line(char symbol, int len);
 
 int main() {
-	setlocale(LC_ALL, "Rus");
-	int a, b, h, max, max_size, kol, cont = 1;
-	 
+	//setlocale(LC_ALL, "Rus");
+	float a, b, h, max;
+	int max_size, kol, cont = 1;
+	
 	do {
 		do {
 			int ch;
 			printf("Введите границы и шаг(a b h): ");
-			kol = scanf("%d%d%d", &a, &b, &h);
+			kol = scanf("%f%f%f", &a, &b, &h);
 			while (ch = getchar() != '\n');
 		} while (kol < 3);
 
-		while (1) {
-			max = a > b ? a : b;
-			if (h != 0 && (max == a && h < 0 || max == b && h > 0)) break;
-			if (h == 0) {
-				printf("Шаг не может быть равен 0!\n");
-				do {
-					int ch;
-					printf("Введите шаг заново: ");
-					kol = scanf("%d", &h);
-					while (ch = getchar() != '\n');
-				} while (kol < 1);
-			}
-			if (max == a && h > 0) {
-				printf("Нельзя двигаться назад с положительным шагом!\nВведите данные заново\n");
-				do {
-					int ch;
-					printf("Введите границы и шаг(a b h): ");
-					kol = scanf("%d%d%d", &a, &b, &h);
-					while (ch = getchar() != '\n');
-				} while (kol < 3);
-			}
-			if (max == b && h < 0) {
-				printf("Нельзя двигаться вперед с отрицательным шагом!\nВведите данные заново\n");
-				do {
-					int ch;
-					printf("Введите границы и шаг(a b h): ");
-					kol = scanf("%d%d%d", &a, &b, &h);
-					while (ch = getchar() != '\n');
-				} while (kol < 3);
-			}
-		}
 		max = a > b ? a : b;
+
+		while (1) {
+			if (h > 0) break;
+			printf("Шаг должен быть положительным!\n");
+			do {
+				int ch;
+				printf("Введите шаг заново: ");
+				kol = scanf("%f", &h);
+				while (ch = getchar() != '\n');
+			} while (kol < 1);
+		}
+
 		max_size = number_len(max * max);
 		max_size = max_size > 2 ? max_size : 3;
 
 		printf("\n");
-		print_line('-', max_size * 2 + 3);
+		print_line(HOR_SYMBOL, max_size * 2 + 3);
 		printf("\n");
 		printf("|%*c|%*c|\n", max_size, 'x', max_size, 'y');
-		print_line('-', max_size * 2 + 3);
+		print_line(HOR_SYMBOL, max_size * 2 + 3);
 		printf("\n");
-		for (a; h > 0 ? a <= b : a >= b; a += h) {
-			printf("|%*d|%*d|\n", max_size, a, max_size, a * a);
-			print_line('-', max_size * 2 + 3);
-			printf("\n");
+		
+		if (max == b){
+			for (a; a <= b; a += h) {
+				printf("|%*.*f|%*.*f|\n", max_size, ROUND_COUNT, a, max_size, ROUND_COUNT, a * a);
+				print_line(HOR_SYMBOL, max_size * 2 + 3);
+				printf("\n");
+			}
 		}
+		else {
+			for (a; a >= b; a -= h) {
+				printf("|%*.*f|%*.*f|\n", max_size, ROUND_COUNT, a, max_size, ROUND_COUNT, a * a);
+				print_line(HOR_SYMBOL, max_size * 2 + 3);
+				printf("\n");
+			}
+		}
+
 		printf("\nВведите 1, чтобы продолжить: ");
 		scanf("%d", &cont);
 		printf("\n");
@@ -76,7 +73,7 @@ int number_len(int number) {
 		len++;
 		number /= 10;
 	}
-	return len;
+	return len + ROUND_COUNT + 2;
 }
 
 void print_line(char symbol, int len) {
